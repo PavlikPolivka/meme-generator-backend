f153ef4e60c46bd6de72f117ae5e49ef7980bdd9
Pavel Polivka <pavel.polivka@hey.com>
2024-11-12 00:39:15 +0100

    cros

--- api/generateMeme.js
+++ api/generateMeme.js
@@ -11,6 +11,10 @@ module.exports = async (req, res) => {
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
   const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed, use POST' });
   }
